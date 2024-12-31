1e32f10f9aef5bea1a627286cff14f3c7523b6f0
BnDLifeLight <benjmainolaveson+git@gmail.com>
2024-12-31 10:42:14 -0500

    Added some key Variables for calculations

--- main.cpp
+++ main.cpp
@@ -11,8 +11,14 @@ int main(){
     InitWindow(screenWidth, screenHeight, "A Real Crappy Calulator");
     SetTargetFPS(60);
 
+    // Costants
+    Color VeryLightGrey = { 150, 150, 150, 255 };
+
     // Variables
-    Color VeryLightGrey = { 150, 150, 150, 255 };    
+    float display = 0.0f; // Where the result will output to
+    string displaystr = "0000000000" ; // Constantly set to the str value of "display" in a function
+    float num1 = 0.0f;
+    float num2 = 0.0f;
 
     // Main Loop
     while(!WindowShouldClose()){
