419d0c339b9ebf1dfe6cc67a5ce9d35f0199695c
BnDLifeLight <benjaminolaveson+git@gmail.com>
2024-12-30 21:03:29 -0500

    Created Structure for Easier Code readability in the Future

--- main.cpp
+++ main.cpp
@@ -2,10 +2,31 @@
 #include <string>
 #include "raylib.h"
 
+
 int main(){
 
+    // Initialization
+    const int screenWidth = 500;
+    const int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "A Real Crappy Calulator");
+    SetTargetFPS(60);
+
+    // Variables
+    Color VeryLightGrey = { 150, 150, 150, 255 };    
+
+    // Main Loop
+    while(!WindowShouldClose()){
+        // Drawing
+        BeginDrawing();
+        ClearBackground(VeryLightGrey); // Make the Background a Very light grey
 
 
+
+        EndDrawing();
+    }
+
+    // Cleanup
+    CloseWindow();
 
     return 0;
 }
